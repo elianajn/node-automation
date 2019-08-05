8670caf84f94a33fba274e55edf93d90e0e77052
Eliana Neurohr <elianajneu@gmail.com>
2019-08-05 12:58:51 -0700

    ifcfg file removal

--- boron2.pp
+++ boron2.pp
@@ -2,14 +2,14 @@ $folder="/opt/puppetlabs/puppet/manifests/boron/boron2"
 package { ['ntp', 'ntpdate', 'nfs-utils', 'libibverbs-utils', 'infiniband-diags', 'ibutils', 'perftest', 'libmlx4', 'libibverbs', 'opensm', 'net-tools', 'openmpi', 'openmpi-devel', 'freeipmi', 'rpm-build', 'bzip2-devel', 'perl-ExtUtils-MakeMaker', 'perl-Switch', 'openssl', 'openssl-devel', 'pam-devel', 'readline-devel', 'mariadb-devel', 'mariadb-server'] :
   ensure => 'installed',
 }
-file { '/etc/sysconfig/network-scripts/ifcfg-ib0':
-        ensure => "file",
-        owner  => "root",
-        group  => "root",
-        mode   => "644",
-	seluser => "system_u",
-        source => "$folder/ifcfg-ib0",
-}
+#file { '/etc/sysconfig/network-scripts/ifcfg-ib0':
+#        ensure => "file",
+#        owner  => "root",
+#        group  => "root",
+#        mode   => "644",
+#	seluser => "system_u",
+#        source => "$folder/ifcfg-ib0",
+#}
 file { '/etc/passwd':
         ensure => "file",
         owner  => "root",
@@ -58,21 +58,23 @@ file { '/etc/resolv.conf':
         seluser => "system_u",
         source => "$folder/resolv.conf",
 }
-file { '/etc/sysconfig/network-scripts/ifcfg-em1':
-        ensure => "file",
-        owner  => "root",
-        group  => "root",
-        mode   => "644",
-        seluser => "system_u",
-        source => "$folder/ifcfg-em1",
-}file { '/etc/sysconfig/network-scripts/ifcfg-em2':
-        ensure => "file",
-        owner  => "root",
-        group  => "root",
-        mode   => "644",
-        seluser => "system_u",
-        source => "$folder/ifcfg-em2",
-}file { '/etc/sysctl.conf':
+#file { '/etc/sysconfig/network-scripts/ifcfg-em1':
+#        ensure => "file",
+#        owner  => "root",
+#        group  => "root",
+#        mode   => "644",
+#        seluser => "system_u",
+#        source => "$folder/ifcfg-em1",
+#}
+#file { '/etc/sysconfig/network-scripts/ifcfg-em2':
+#        ensure => "file",
+#        owner  => "root",
+#        group  => "root",
+#        mode   => "644",
+#        seluser => "system_u",
+#        source => "$folder/ifcfg-em2",
+#}
+file { '/etc/sysctl.conf':
         ensure => "file",
         owner  => "root",
         group  => "root",
